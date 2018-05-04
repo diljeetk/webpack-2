cb348aacbf447e9551d922504d8e535eed787a16
Diljeet Kakkar <diljeet.kakkar@puresoft.int>
2018-05-04 18:45:38 +0530

    4. integrated way to work with js files using babel-loader babel-core in webpackConfigJS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import './css/main.css';
 import './scss/main.scss';
+import {hello, sup} from './js/module';
 
 console.log("Webpack 2");
-console.log("Webpack 2");
-console.log("Webpack 2");
+hello();
+sup();
+console.log("Hello from index.js");
+
 
