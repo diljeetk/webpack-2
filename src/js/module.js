ddbb736ac9c84a126314b39da657d57ae4ac00bd
diljeetk <diljeetkakkarw@gmail.com>
2018-05-05 15:38:51 +0530

    10. Webpack-dev-server auto reloading

--- src/js/module.js
+++ src/js/module.js
@@ -3,7 +3,7 @@ function hello(){
 }
 
 function sup(){
-    console.log("Hello from JS sup");
+    console.log("Hello from JS sup from module.js checking automatic reloading");
 }
 
 export {hello, sup}
