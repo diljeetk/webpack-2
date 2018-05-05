fa6305444e3644b0bc6e66885bff4c0cbba6f814
diljeetk <diljeetkakkarw@gmail.com>
2018-05-05 17:51:55 +0530

    11. handling multiple files with browserSync

--- webpack.config.js
+++ webpack.config.js
@@ -57,74 +57,6 @@ module.exports = {
        // new CleanWebpackPlugin(['dist']),  // telling which folder to delete 
       new ExtractTextPlugin("styles.css"), // specifying the file name seperatly for css
       new HtmlWebpackPlugin({
-          title:'Learning Webpack 2'
-      }), // creating index.html automatically while npm run build 
-      new BrowserSyncPlugin(
-        // BrowserSync options
-        {
-          // browse to http://localhost:3000/ during development
-          host: 'localhost',
-          port: 3000,
-          // proxy the Webpack Dev Server endpoint
-          // (which should be serving on http://localhost:3100/)
-          // through BrowserSync
-          proxy: 'http://localhost:8080/'
-        },
-        // plugin options
-        {
-          // prevent BrowserSync from reloading the page
-          // and let Webpack Dev Server take care of this
-          reload: false
-        }
-      )
-
-    ]
-}
-/*
-module.exports = {
-    entry: {
-      app: './src/index.js',
-      about: './src/js/about.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist')
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-      contentBase: './dist'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: ['css-loader', 'postcss-loader']
-          })
-        },
-        {
-          test: /\.scss$/,
-          use: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: [
-              {loader: 'css-loader', options: {sourceMap: true} },
-              {loader: 'postcss-loader', options: {sourceMap: true} },
-              {loader: 'sass-loader', options: {sourceMap: true} }
-            ]
-          })
-        },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader'
-        }
-      ]
-    },
-    plugins: [
-      new CleanWebpackPlugin(['dist']),
-      new ExtractTextPlugin('styles.css'),
-      new HtmlWebpackPlugin({
         filename: 'index.html',
         template: 'src/views/index.html',
         chunks: ['app']
@@ -152,6 +84,6 @@ module.exports = {
           reload: false
         }
       )
+
     ]
-  };
-  */
+}
